f942a7e24379b82bfecea6b2a6e42dfa3daa18b7
T_NahuelReboledo <thiagoreboledo8@gmail.com>
2023-11-08 15:02:13 -0300

    fs commit add Tailwind config

---
+++ Client/src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+   return (
+      <section>
+         <h1>Header</h1>
+         <h2>Header</h2>
+         <h3>Header</h3>
+      </section>
+   );
+}
+
+export default Header;
